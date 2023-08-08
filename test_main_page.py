0f7284b11a25cce10cd54080cf7a94213b0e6e5a
Alecs <alecsanderbejnar@gmail.com>
2023-08-08 12:34:07 +0300

    main_page class

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,4 @@
 # pytest -v --tb=line --language=en test_main_page.py
-from selenium.webdriver.common.by import By
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
 
 
 def test_guest_can_go_to_login_page(browser):
