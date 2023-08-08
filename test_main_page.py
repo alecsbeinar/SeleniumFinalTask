0f8843fd03e1378c12f9ae425d97a86cb90a2954
Alecs <alecsanderbejnar@gmail.com>
2023-08-08 12:37:35 +0300

    page_object style

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,12 @@
 # pytest -v --tb=line --language=en test_main_page.py
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
 # def test_add_to_cart(browser):
 #     page = ProductPage(url="", browser)   # инициализируем объект Page Object
